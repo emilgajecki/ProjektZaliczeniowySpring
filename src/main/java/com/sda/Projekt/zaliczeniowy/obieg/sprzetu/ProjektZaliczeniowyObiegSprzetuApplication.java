e46e484ab17ad4b06540f3bbe602827e7e1f143a
emilgajecki <60809250+emilgajecki@users.noreply.github.com>
2020-10-11 12:41:17 +0200

    Projekt zaliczeniowy

---
+++ src/main/java/com/sda/Projekt/zaliczeniowy/obieg/sprzetu/ProjektZaliczeniowyObiegSprzetuApplication.java
@@ -0,0 +1,13 @@
+package com.sda.Projekt.zaliczeniowy.obieg.sprzetu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektZaliczeniowyObiegSprzetuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektZaliczeniowyObiegSprzetuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/Projekt/zaliczeniowy/obieg/sprzetu/ProjektZaliczeniowyObiegSprzetuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.Projekt.zaliczeniowy.obieg.sprzetu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektZaliczeniowyObiegSprzetuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
