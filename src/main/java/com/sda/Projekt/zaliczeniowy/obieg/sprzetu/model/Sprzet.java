e06cfd49ca6348017c6a9f147f0f7668fbe5fd1a
emilgajecki <60809250+emilgajecki@users.noreply.github.com>
2020-10-11 13:12:57 +0200

    utworzone encje + login to database

---
+++ src/main/java/com/sda/Projekt/zaliczeniowy/obieg/sprzetu/model/Sprzet.java
@@ -0,0 +1,42 @@
+package com.sda.Projekt.zaliczeniowy.obieg.sprzetu.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Sprzet {
+
+    @Id
+    //nie będzie powielało id.
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nazwaSprzetu;
+    private String kategoriaSprzetu;
+
+    public Sprzet(){
+
+    }
+
+    public Sprzet(String nazwaSprzetu, String kategoriaSprzetu) {
+        this.nazwaSprzetu = nazwaSprzetu;
+        this.kategoriaSprzetu = kategoriaSprzetu;
+    }
+
+    public String getNazwaSprzetu() {
+        return nazwaSprzetu;
+    }
+
+    public void setNazwaSprzetu(String nazwaSprzetu) {
+        this.nazwaSprzetu = nazwaSprzetu;
+    }
+
+    public String getKategoriaSprzetu() {
+        return kategoriaSprzetu;
+    }
+
+    public void setKategoriaSprzetu(String kategoriaSprzetu) {
+        this.kategoriaSprzetu = kategoriaSprzetu;
+    }
+}
